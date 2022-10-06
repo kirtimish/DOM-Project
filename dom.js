0301f4127f612ec9a41db003a9555b40fd9dada3
kirtimish <kirtimishra0708@gmail.com>
2022-10-07 03:56:38 +0530

    DOM-basics

---
+++ dom.js
@@ -0,0 +1,24 @@
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = "DOM";
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[20]);
+// document.all[20].textContent = "lastname";
+// console.log(document.forms[0]);
+
+// console.log(document.links);
+
+// var header = document.getElementById("main-header");
+// console.log(header);
+// header.textContent = "Hello";
+// header.innerText = "goodbye";
+
+// header.style.borderBottom = 'solid 3px black';
+var headtitle = document.querySelector(".title");
+headtitle.style.fontWeight = "bold";
+headtitle.style.color = "green";
